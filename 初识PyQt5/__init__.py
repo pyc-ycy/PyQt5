e7570ec29ccdabc9d304f110ec5498b060f919c4
御承扬 <2923616405@qq.com>
2019-06-23 17:42:46 +0800

    Update

---
+++ 初识PyQt5/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3.7
+# encoding: utf-8
+# Time : 2019/6/23 15:10 
+# Author : 御承扬
+# e-mail:2923616405@qq.com
+# project:  PyQt5
+# File : __init__.py.py 
+# @software: PyCharm
---
+++ 测试专用/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python3.7
+# encoding: utf-8
+# site:D:\users\lenovo\PycharmProjects\untitled
+# Time : 2019/6/23 14:52 
+# Author : 御承扬
+# e-mail:2923616405@qq.com
+# project:  PyQt5
+# File : __init__.py.py 
+# @oftware: PyCharm

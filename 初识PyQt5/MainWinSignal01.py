8288e7515b38dd5e015b82d2278a5d0d2a9fe888
御承扬 <2923616405@qq.com>
2019-07-24 15:09:26 +0800

    Update

--- 初识PyQt5/MainWinSignal01.py
+++ 初识PyQt5/MainWinSignal01.py
@@ -8,7 +8,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_Form01(object):
     def setupUi(self, Form01):
         Form01.setObjectName("Form01")
@@ -16,12 +15,27 @@ class Ui_Form01(object):
         self.closeWinBtn = QtWidgets.QPushButton(Form01)
         self.closeWinBtn.setGeometry(QtCore.QRect(170, 170, 93, 28))
         self.closeWinBtn.setObjectName("closeWinBtn")
+        self.checkBox = QtWidgets.QCheckBox(Form01)
+        self.checkBox.setGeometry(QtCore.QRect(60, 170, 91, 19))
+        self.checkBox.setObjectName("checkBox")
+        self.label = QtWidgets.QLabel(Form01)
+        self.label.setGeometry(QtCore.QRect(60, 280, 72, 15))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(Form01)
+        self.lineEdit.setGeometry(QtCore.QRect(200, 280, 113, 21))
+        self.lineEdit.setObjectName("lineEdit")
 
         self.retranslateUi(Form01)
         self.closeWinBtn.clicked.connect(Form01.close)
+        self.checkBox.clicked['bool'].connect(self.label.setVisible)
+        self.checkBox.clicked['bool'].connect(self.lineEdit.setEnabled)
         QtCore.QMetaObject.connectSlotsByName(Form01)
 
     def retranslateUi(self, Form01):
         _translate = QtCore.QCoreApplication.translate
         Form01.setWindowTitle(_translate("Form01", "Form"))
         self.closeWinBtn.setText(_translate("Form01", "关闭窗口"))
+        self.checkBox.setText(_translate("Form01", "选择"))
+        self.label.setText(_translate("Form01", "显示1"))
+        self.lineEdit.setText(_translate("Form01", "显示2"))
+
